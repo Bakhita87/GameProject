03649ed7e1f7d7ed9250c3200ba1a7753f75d740
Bakhita87 <bakhitaotieno@gmail.com>
2023-12-22 17:08:09 +0300

    Created a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,40 +1,44 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+// import { Text } from "@chakra-ui/react";
+// import useGames from "../hooks/useGames";
+
+// const GameGrid = () => {
+   
+//     const {games,error} =  useGames ();
+
+//     return(
+
+//         <>
+//          {error && <Text>error</Text>}
+
+
+//        <ul>
+//          {games.map((game) => (
+//          <li key={game.id}>{game.name}</li>
+//          ))}
+//        </ul>
+//        </>
+//     )
+// }
+
+// export default GameGrid;
+
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-   id: number;
-   name: string;
-}
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[]
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-   const [games, setGames] = useState<Game[]>([]);
-   const [error, setError] =  useState('');
+  const { games, error } = useGames();
 
-   useEffect(() =>{
-      apiClient.get<FetchGamesResponse>('/xgames')
-        .then(res=> setGames(res.data.results))
-        .catch(err => setError(err.message));
-   })
+  return (
+    <>
+      {error && <Text>{error}</Text>}
 
-
-    return(
-
-        <>
-         {error && <Text>error</Text>}
-
-
-       <ul>
-        {games.map(game => <li key={game.id}>{game.name}</li>)}
-       </ul>
-       </>
-    )
-}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default GameGrid;
-
